467a45e951d6a643ae88db494e07d6d1ecc3a896
Andre <andre@homepad.com>
2020-10-13 09:40:00 +0200

    Create base select component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <select
+    :value="modelValue"
+    class="field"
+    v-bind="{
+      ...$attrs,
+      onChange: ($event) => {
+        $emit('update:modelValue', $event.target.value)
+      }
+    }"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >{{ option }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    // default v-model prop from vue 3
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    options: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -3,15 +3,11 @@
     <h1>Create an event</h1>
     <form>
 
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option
-          v-for="option in categories"
-          :value="option"
-          :key="option"
-          :selected="option === event.category"
-        >{{ option }}</option>
-      </select>
+      <BaseSelect
+        v-model="event.category"
+        label="Select a category"
+        :options="categories"
+      ></BaseSelect>
 
       <h3>Name & describe your event</h3>
 
